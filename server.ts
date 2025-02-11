9422550b7767a247fdc8ed1510279dfa1957f289
ilhamnrfdn <ilhamnoer@femaledaily.com>
2025-02-12 02:46:19 +0700

    first commit

---
+++ server.ts
@@ -0,0 +1,26 @@
+// server.js
+const { spawn } = require("child_process");
+const path = require("path");
+
+// running backend
+const backend = spawn("yarn", ["server:dev"], {
+	cwd: path.join(__dirname, "/apps/backend-repo"),
+});
+backend.stdout.on("data", (data) => {
+	console.log(`Backend: ${data}`);
+});
+backend.stderr.on("data", (data) => {
+	console.error(`Backend error: ${data}`);
+});
+
+// running frontend (Next.js)
+const frontend = spawn("yarn", ["server:dev"], {
+	cwd: path.join(__dirname, "/apps/front-repo"),
+});
+
+frontend.stdout.on("data", (data) => {
+	console.log(`Frontend: ${data}`);
+});
+frontend.stderr.on("data", (data) => {
+	console.error(`Frontend error: ${data}`);
+});
